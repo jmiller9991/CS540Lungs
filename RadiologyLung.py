480b1a2536ba5e3b630373976919af10e81bdb8d
jmiller9991 <jacobmiller9991@gmail.com>
2020-03-09 15:50:33 -0400

    updated split method etc

--- RadiologyLung.py
+++ RadiologyLung.py
@@ -1,5 +1,4 @@
 import sys
-import glob
 import os
 from joblib import Parallel, delayed
 import pickle
@@ -16,57 +15,99 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 import tensorflow.keras.utils
+import random
+import math as m
 
 raw_image_path = 'Data/samples/'
 output_directory = 'Data\\outputtojpg'
 candidate_file = 'Data\\candidates.csv'
 X_log_train = []
 X_log_test = []
-X_log_val = []
 
-class MHDConverter(object):
+class CTScan(object):
+    """
+	A class that allows you to read .mhd header data, crop images and
+	generate and save cropped images
+
+    Args:
+    filename: .mhd filename
+    coords: a numpy array
+	"""
+
     def __init__(self, filename=None, coords=None, path=None):
+        """
+        Args
+        -----
+        filename: .mhd filename
+        coords: coordinates to crop around
+        ds: data structure that contains CT header data like resolution etc
+        path: path to directory with all the raw data
+        """
         self.filename = filename
         self.coords = coords
         self.ds = None
-        self.img = None
+        self.image = None
         self.path = path
 
     def reset_coords(self, coords):
+        """
+        updates to new coordinates
+        """
         self.coords = coords
 
-    def read_mhd_img(self):
-        #path = glob.glob(self.path + self.filename + '.mdh')
-        path = [raw_image_path+name for name in os.listdir(raw_image_path) if name.endswith(".mhd")]
+    def read_mhd_image(self):
+        """
+        Reads mhd data
+        """
+        # path = glob.glob(self.path + self.filename + '.mhd')
+        path = [raw_image_path + name for name in os.listdir(raw_image_path) if name.endswith(".mhd")]
         self.ds = sitk.ReadImage(path[0])
         self.image = sitk.GetArrayFromImage(self.ds)
 
     def get_voxel_coords(self):
+        """
+        Converts cartesian to voxel coordinates
+        """
         origin = self.ds.GetOrigin()
         resolution = self.ds.GetSpacing()
-        voxel_coords = [np.absolute(self.coords[j]-origin[j])/resolution for j in range(len(self.coords))]
+        voxel_coords = [np.absolute(self.coords[j] - origin[j]) / resolution[j] \
+                        for j in range(len(self.coords))]
         return tuple(voxel_coords)
 
-    def get_subimg(self, width):
-        self.read_mhd_img()
+    def get_image(self):
+        """
+        Returns axial CT slice
+        """
+        return self.image
+
+    def get_subimage(self, width):
+        """
+        Returns cropped image of requested dimensiona
+        """
+        self.read_mhd_image()
         x, y, z = self.get_voxel_coords()
-        print(self.image)
-        subImg = self.image[int(z), int(y-width/2):int(y+width/2), int(x-width/2):int(x+width/2)]
-        return subImg
+        subImage = self.image[int(z), int(y - width / 2):int(y + width / 2), \
+                   int(x - width / 2):int(x + width / 2)]
+        return subImage
 
     def normalizePlanes(self, npzarray):
-        maxHU = 400
-        minHU = -1000
-        npzarray = (npzarray - minHU)/ (maxHU - minHU)
-        npzarray[npzarray>1] = 1
-        npzarray[npzarray<0] = 0
+        """
+        Copied from SITK tutorial converting Houndsunits to grayscale units
+        """
+        maxHU = 400.
+        minHU = -1000.
+        npzarray = (npzarray - minHU) / (maxHU - minHU)
+        npzarray[npzarray > 1] = 1.
+        npzarray[npzarray < 0] = 0.
         return npzarray
 
-    def save_img(self, filename, width):
-        img = self.get_subimg(width)
-        img = self.normalizePlanes(img)
-        Image.fromarray(img*255).convert('L').save(filename)
-
+    def save_image(self, filename, width):
+        """
+        Saves cropped CT image
+        """
+        image = self.get_subimage(width)
+        image = self.normalizePlanes(image)
+        Image.fromarray(image * 255).convert('L').save(filename)
 
 
 def log(image):
@@ -116,30 +157,40 @@ def computeFeatures(image, regionSideCount, feature):
             index += 36
 '''
 
-def convert_data(idx, outDir, X_data, width=50):
-    converter = MHDConverter(np.asarray(X_data.loc[idx])[0], np.asarray(X_data.loc[idx])[1:], raw_image_path)
+def create_data(idx, outDir, X_data, width=50):
+    '''
+    Generates your test, train, validation images
+    outDir = a string representing destination
+    width (int) specify image size
+    '''
+    scan = CTScan(np.asarray(X_data.loc[idx])[0], \
+                  np.asarray(X_data.loc[idx])[1:], raw_image_path)
     outfile = outDir + str(idx) + '.jpg'
-    converter.save_img(outfile, width)
+    scan.save_image(outfile, width)
+
 
 def do_test_train_split(filename):
     """
     Does a test train split if not previously done
+
     """
     candidates = pd.read_csv(filename)
 
-    positives = candidates[candidates['class']==1].index
-    negatives = candidates[candidates['class']==0].index
+    positives = candidates[candidates['class'] == 1].index
+    negatives = candidates[candidates['class'] == 0].index
 
     ## Under Sample Negative Indexes
     np.random.seed(42)
-    negIndexes = np.random.choice(negatives, len(positives)*5, replace=False)
+    negIndexes = np.random.choice(negatives, len(positives) * 5, replace=False)
 
-    candidatesDf = candidates.iloc[list(positives)+list(negIndexes)]
+    candidatesDf = candidates.iloc[list(positives) + list(negIndexes)]
 
-    X = candidatesDf.iloc[:,:-1]
-    y = candidatesDf.iloc[:,-1]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
+    X = candidatesDf.iloc[:, :-1]
+    y = candidatesDf.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, \
+                                                        test_size=0.25, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, \
+                                                      test_size=0.25, random_state=42)
 
     X_train.to_pickle('traindata')
     y_train.to_pickle('trainlabels')
@@ -148,8 +199,9 @@ def do_test_train_split(filename):
     X_val.to_pickle('valdata')
     y_val.to_pickle('vallabels')
 
+
 def buildModel(inputShape):
-    model = ks.Sequential()
+    model = Sequential()
     model.add(Conv2D(filters=64, kernel_size=3, activation='relu', input_shape=inputShape))
     model.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
     model.add(Conv2D(filters=64, kernel_size=3, activation='relu'))
@@ -171,6 +223,29 @@ def buildModel(inputShape):
 
     return model, batchSize, epochs
 
+
+def splitLogMethod(dir):
+    X_data_orig = []
+    X_train = []
+    X_test = []
+    for f in os.listdir(dir):
+        f.open('r')
+        X_data_orig.append(log(f))
+    cv.imshow("Logs", X_data_orig[0])
+
+    random.seed(420)
+    for i in range(m.floor(X_data_orig.__sizeof__()*0.75)):
+        temp1 = random.choice(X_data_orig)
+        X_data_orig.remove(temp1)
+        X_train.append(temp1)
+
+    for i in range(X_data_orig.__sizeof__()):
+        temp2 = random.choice(X_data_orig)
+        X_data_orig.remove(temp2)
+        X_test.append(temp2)
+
+    return X_train, X_test
+
 def main():
     if len(sys.argv) < 2:
         raise ValueError('1 argument needed. Specify if you need to generate a train, test or val set')
@@ -187,13 +262,11 @@ def main():
     else:
         do_test_train_split(candidate_file)
     X_data = pd.read_pickle(inpfile)
-    Parallel(n_jobs=3)(delayed(convert_data)(idx, outDir, X_data) for idx in X_data.index)
-    for f in os.listdir(outDir):
-        img = f.open()
-        cv.imshow("filename", img)
-        #do_train_test_split(f)
+    Parallel(n_jobs=3)(delayed(create_data)(idx, outDir, X_data) for idx in X_data.index)
+    
+    x_log_train, x_log_test = splitLogMethod(outDir)
 
-    model, batchSize, epochs = buildModel('''Insert Image Size Here''')
+    model, batchSize, epochs = buildModel((50, 50))
     model.summary()
     model.fit(x_log_train, x_log_test, batch_size=batchSize, epochs=epochs, validation_split=0.25)
 
